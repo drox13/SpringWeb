4c71fb651fd53ad9ad8cdf4c8f0b61f132d732b9
drox13 <dario.baron@uptc.edu.co>
2024-09-20 17:36:18 -0500

    add commets

--- src/main/java/com/dario/curso/sprintboot/webapp/sprintboot_web/controllers/PathVariableController.java
+++ src/main/java/com/dario/curso/sprintboot/webapp/sprintboot_web/controllers/PathVariableController.java
@@ -14,10 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
-
 
     @RestController
     @RequestMapping("/api/var")
@@ -35,7 +31,7 @@ import org.springframework.web.bind.annotation.RequestParam;
         @Value("${config.code}")
         private Integer code;
 
-    @GetMapping("/baz/{message}")
+    @GetMapping("/baz/{message}") //Path variable
     public ParamDto baz(@PathVariable String message) {
         ParamDto param = new ParamDto();
         param.setMessage("este es con PathVariable " + message);
